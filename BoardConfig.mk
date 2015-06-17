ada24d32ae1a4196ca3b99176778ee34dff5be1a
Patrick Graber <pjgraber03@gmail.com>
2015-06-16 21:41:04 -0700

    add bliss build options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,3 +192,27 @@ INTEL_USE_CAMERA_UVC := true
 
 # Recipes to generate prebuilts
 -include device/intel/common/external/external.mk
+
+# Blissiful building options
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_GRAPHITE := true
+BLISS_STRICT := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+Link_Time_Optimizations := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
